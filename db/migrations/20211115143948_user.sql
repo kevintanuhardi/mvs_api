ab25909808339de57739ed528d995d736d685101
Kevin Tanuhardi <kevin.tanuhardi@gmail.com>
2022-01-01 20:10:38 +0700

    ADD: user migration

--- db/migrations/20211115143948_user.sql
+++ db/migrations/20211115143948_user.sql
@@ -2,14 +2,12 @@
 CREATE TABLE `user`
 (
     `id`            int(11)      NOT NULL AUTO_INCREMENT,
-    `employee_id`   varchar(40)      NOT NULL,
-    `company_id`		int(11) NOT NULL,
     `name`   varchar(63)      NOT NULL,
-    `active`        boolean			  NOT NULL,
+    `is_active`        boolean			  NOT NULL,
     `phone_number`	varchar(25)			  NOT NULL,
     `email`					varchar(40)			  NOT NULL,
     `password`			varchar(255)			  NOT NULL,
-    `role_id`			int(11)			  NOT NULL,
+    `role`			enum('admin', 'super_admin')			  NOT NULL,
     `created_at`    Timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`    Timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
